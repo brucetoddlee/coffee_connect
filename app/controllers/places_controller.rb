0dbe0faba0e981db7449742ecfa49ef284eaaac6
brucetoddlee <brucetlee@gmail.com>
2014-02-06 22:07:10 -0800

    added link to Google Maps

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,7 +11,7 @@ class PlacesController < ApplicationController
   	consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
   	access_token = OAuth::AccessToken.new(consumer, token, token_secret)
 
-    path = "/v2/search?term=coffee+shop&location=#{params[:zip_code]}"
+    path = "/v2/search?term=coffee&location=#{params[:zip_code]}&attrs=WiFi.free"
 
   	api_request = access_token.get(path).body
   	@results = JSON.parse(api_request)
@@ -53,7 +53,6 @@ class PlacesController < ApplicationController
     api_request_by_id = access_token.get(path).body
     @place = JSON.parse(api_request_by_id)
 
-
     tests_count = Test.where(yelp_id: id).count
 
     if tests_count == 0
@@ -67,28 +66,21 @@ class PlacesController < ApplicationController
       download_array = Test.where(yelp_id: id).map do |el| 
         el["download"]
       end
-
       avg_download = (download_array.sum / tests_count).round(1)
-
 
       upload_array = Test.where(yelp_id: id).map do |el| 
         el["upload"]
       end
-
       avg_upload = (upload_array.sum / tests_count).round(1)
-
 
       latency_array = Test.where(yelp_id: id).map do |el| 
         el["latency"]
       end
-
       avg_latency = (latency_array.sum / tests_count).round(1)
       
-
       jitter_array = Test.where(yelp_id: id).map do |el| 
         el["jitter"]
       end
-
       avg_jitter = (jitter_array.sum / tests_count).round(1)
 
     end
@@ -98,6 +90,8 @@ class PlacesController < ApplicationController
     @place["avg_upload"] = avg_upload
     @place["avg_latency"] = avg_latency
     @place["avg_jitter"] = avg_jitter
+
+    @google_maps_address = @place["location"]["display_address"][0].gsub(" ", "+") + "+" + @place["location"]["postal_code"]
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 CoffeeConnect::Application.routes.draw do
   
-  root 'welcome#index'
+  root "welcome#index"
 
-  get '/places/index' => 'places#index', as: :index
+  get "/places/index" => "places#index", as: :index
 
-  get '/places/:id' => 'places#show', as: :show
+  get "/places/:id" => "places#show", as: :show
 
   get "/:yelp_id", to: "places#show", as: "place"
 
