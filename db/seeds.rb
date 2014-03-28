3ba2196999b35352e06f9f35b441ccb12a7935d8
brucetoddlee <brucetlee@gmail.com>
2014-03-28 01:36:24 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,3 +29,78 @@ tests << Test.create(yelp_id: "coffeeshop-san-francisco", download: "3.0", uploa
 tests << Test.create(yelp_id: "coffeeshop-san-francisco", download: "3.2", upload: "1.5", latency:"18", jitter:"20")
 tests << Test.create(yelp_id: "coffeeshop-san-francisco", download: "4.0", upload: "2.0", latency:"15", jitter:"15")
 tests << Test.create(yelp_id: "coffeeshop-san-francisco", download: "5.0", upload: "3.0", latency:"11", jitter:"10")
+
+
+
+tests << Test.create(yelp_id: "contraband-coffee-bar-san-francisco", download: "5.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "contraband-coffee-bar-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "philz-coffee-san-francisco-4", download: "1.0", upload: "0.5", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "philz-coffee-san-francisco-4", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "saint-frank-coffee-san-francisco-2", download: "4.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "saint-frank-coffee-san-francisco-2", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "beacon-coffee-and-pantry-san-francisco", download: "8.0", upload: "3.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "beacon-coffee-and-pantry-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "its-a-grind-coffee-house-san-francisco", download: "9.0", upload: "3.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "its-a-grind-coffee-house-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "jane-on-fillmore-san-francisco", download: "4.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "jane-on-fillmore-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "another-cafe-san-francisco", download: "9.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "another-cafe-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "quetzal-internet-cafe-san-francisco", download: "6.0", upload: "3.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "quetzal-internet-cafe-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "the-chameleon-cafe-san-francisco", download: "5.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "the-chameleon-cafe-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "moka-coffee-san-francisco", download: "9.0", upload: "3.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "moka-coffee-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "peets-coffee-and-tea-san-francisco-14", download: "8.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "peets-coffee-and-tea-san-francisco-14", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "peets-coffee-and-tea-san-francisco-2", download: "6.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "peets-coffee-and-tea-san-francisco-2", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "the-brew-san-francisco", download: "7.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "the-brew-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "royal-ground-coffees-san-francisco", download: "12.0", upload: "6.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "royal-ground-coffees-san-francisco", download: "11.3", upload: "5.8", latency:"12", jitter:"13")
+
+
+
+tests << Test.create(yelp_id: "epicenter-cafe-san-francisco", download: "4.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "epicenter-cafe-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "blue-bottle-coffee-co-san-francisco-7", download: "8.0", upload: "3.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "blue-bottle-coffee-co-san-francisco-7", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "bravado-italian-coffee-bar-and-lounge-san-francisco", download: "9.0", upload: "3.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "bravado-italian-coffee-bar-and-lounge-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "the-creamery-san-francisco-3", download: "4.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "the-creamery-san-francisco-3", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "sightglass-coffee-san-francisco", download: "9.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "sightglass-coffee-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "elite-audio-coffee-bar-san-francisco", download: "6.0", upload: "3.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "elite-audio-coffee-bar-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "coffee-choice-san-francisco", download: "5.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "coffee-choice-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "wicked-grounds-san-francisco", download: "9.0", upload: "3.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "wicked-grounds-san-francisco", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+tests << Test.create(yelp_id: "showplace-caffè-san-francisco-2", download: "8.0", upload: "2.0", latency:"15", jitter:"15")
+tests << Test.create(yelp_id: "showplace-caffè-san-francisco-2", download: "2.2", upload: "1.1", latency:"20", jitter:"19")
+
+
